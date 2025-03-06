161668a71b10c94b23725ce0bf274426baf51344
brey-sev12 <aubrey.sevilla@dmc.edu.ph>
2025-03-06 15:55:56 +0800

    Added 2 student properties: sex and CivilStatus

--- StudentInfo/Models/Student.cs
+++ StudentInfo/Models/Student.cs
@@ -11,6 +11,8 @@ namespace StudentInfo
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
+        public string Sex { get; set; }
+        public string CivilStatus { get; set; }
         public string Contact { get; set; }
         public string Address { get; set; }
     }
